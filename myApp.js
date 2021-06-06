004c68114e5213d608c955a439694677f8d051d3
Petar Ivanov <peteriv9606@gmail.com>
2021-06-06 22:45:51 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const mySecret = process.env["MESSAGE_STYLE"];
+var assets = __dirname + "/public";
+app.use("/public", express.static(assets));
+var home = __dirname + "/views/index.html";
+app.get("/", function (req, res) {
+  res.sendFile(home);
+});
+app.get("/json", function (req, res) {
+  var respone = "Hello JSON";
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    response = response.toUpperCase();
+    res.json({ message: response });
+  } else {
+    res.json({ message: response });
+  }
+});
+app.get("/json", function (req, res) {
+  var respone = "Hello JSON";
 
+  res.json({ message: response });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
